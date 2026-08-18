a3836843ff9dbfd16d9d2568c7a13a97a6976a5b
3miliejoseph <emilieneha@gmail.com>
2026-08-18 13:00:31 -0700

    Update Google Drive link in Root component

--- src/app/components/Root.tsx
+++ src/app/components/Root.tsx
@@ -207,7 +207,7 @@ function RootContent() {
                     </svg>
                   </a>
                   <a
-                    href="https://drive.google.com/file/d/1RWPwW1SifZBGeLDluvIZW8TP8rT9b9Ej/view?usp=sharing"
+                    href="https://drive.google.com/file/d/1AiXEuq4AvvgARkZyb97apd1JqpZTUuHs/view?usp=sharing"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="transition-colors flex items-center"
